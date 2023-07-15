43daf537f1258433a0730026cfe092c072faa2d2
Yash Bhardwaj <33004136+yashbhardwaj26@users.noreply.github.com>
2023-07-15 19:24:26 +0530

    Update CircuitBreakerController.java

--- src/main/java/com/CurrencyExchange/CircuitBreakerController.java
+++ src/main/java/com/CurrencyExchange/CircuitBreakerController.java
@@ -1,17 +1,24 @@
 package com.CurrencyExchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class CircuitBreakerController {
 	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@GetMapping("/sample-api")
+	@Retry(name="default")
 	public String getExampler() {
 		
-		
+		logger.info("Sample api call received");
 	RestTemplate rt = 	new RestTemplate();
 		ResponseEntity<String> forEntity = rt.getForEntity("localHOst:8080/String", String.class);
 		return forEntity.getBody();
